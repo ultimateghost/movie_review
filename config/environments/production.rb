827c5c30e9b144c89e9270bf4022805aa9edef37
Shih Hsiang Wang <ultimateghost@gmail.com>
2016-05-10 13:39:36 +0800

    bug fixed for aws

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,11 +16,11 @@ Rails.application.configure do
   # For paperclip
   config.paperclip_defaults = {
     :storage => :s3,
-    # :s3_host_name => :ap-northeast-1,
     :s3_credentials => {
       :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+      :s3_region => ENV['AWS_REGION']
     }
   }
 
--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,3 +1,4 @@
 Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
 Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:hash.:extension'
 Paperclip::Attachment.default_options[:hash_secret] = 'get_use_rake_secret'
+Paperclip::Attachment.default_options[:s3_host_name] = 's3-ap-northeast-1.amazonaws.com'
